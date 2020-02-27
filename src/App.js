212d85b934dae0ba97a2bb1dc190a7d6c042df6d
Nicolás Gómez <nigomezgu@unal.edu.co>
2020-02-27 15:41:43 -0500

    Bringing routes.js to App.js like in Actas.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import Routes from './js/routes'
-import UnalCanvas from './js/UnalCanvas'
+import { Route, Switch, Redirect } from 'react-router-dom'
+
+import LoginForm from "./js/Components/Login/Login";
+import Contact from "./js/Components/Contact/Contact";
+import Home from "./js/Components/Contact/Contact"
+import RecFormat from "./js/Components/RecFormat/RecFormat"
+import AdminUsers from "./js/Components/AdminUsers/AdminUsers"
+import AdminPrograms from "./js/Components/AdminPrograms/AdminPrograms"
+import GenerateReport from "./js/Components/GenerateReport/GenerateReport"
+import UnalCanvas from "./js/UnalCanvas"
+
+import { ProtectedRoute } from './js/ProtectedRoute'
+import { LoginRoute } from './js/LoginRoute'
+import auth from './auth'
 
 class App extends Component {
   render() {
     return (
         <UnalCanvas>
-          <Routes />
+              <Switch>
+                <LoginRoute exact path="/" component={LoginForm} />
+                <LoginRoute exact path="/contact" component={Contact} />
+                <ProtectedRoute exact path="/home" component={Home} />
+                <ProtectedRoute exact path="/admin_users" component={AdminUsers} />
+                <ProtectedRoute exact path="/admin_programs" component={AdminPrograms} />
+                <ProtectedRoute exact path="/generate_report" component={GenerateReport} />
+                <ProtectedRoute exact path="/rec_format" component={RecFormat} />
+
+                <Route path="*" component={() => "404 NOT FOUND"}>
+                    {auth.isAuthenticated ? <Redirect to="/home" /> : <Redirect to="/" />}
+                </Route> /> 
+            </Switch>
         </UnalCanvas>
     );
   }
