2a0a494fb79d137e09a256aa48cd21561c6d6177
Nicolás Gómez <nigomezgu@unal.edu.co>
2020-03-10 16:40:52 -0500

    Main menu component beta.

--- src/js/Components/Home.js
+++ src/js/Components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "antd/dist/antd.css";
 import { withRouter } from "react-router-dom";
 import { Row, Col } from "antd";
+import MainMenu from "./MainMenu";
 
 class Home extends React.Component {
   render() {
@@ -9,7 +10,7 @@ class Home extends React.Component {
       <Row className="main-row-container">
         <Col xs={2} sm={2} md={4} lg={4} xl={4}></Col>
         <Col xs={20} sm={20} md={16} lg={16} xl={16}>
-          Here will be the content.
+          <MainMenu />
         </Col>
         <Col xs={2} sm={2} md={4} lg={4} xl={4}></Col>
       </Row>
