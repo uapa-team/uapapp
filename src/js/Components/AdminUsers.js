489a9901e128ae5420ec6daa11dc3022ef135da4
Nicolás Gómez <nigomezgu@unal.edu.co>
2020-03-10 18:24:20 -0500

    Improvements in selectors styles while creating types of users.

--- src/js/Components/AdminUsers.js
+++ src/js/Components/AdminUsers.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router-dom"
-import { Table, Input, Button } from "antd";
+import { Form, Radio, Table, Input, Button } from "antd";
 import Highlighter from 'react-highlight-words';
 import { SearchOutlined } from '@ant-design/icons';
 
@@ -84,41 +84,49 @@ class AdminUsers extends React.Component {
                 title: "Nombre",
                 dataIndex: "name",
                 key: "name",
-                width: "45%",
+                width: "40%",
                 ...this.getColumnSearchProps("name", "nombre")
             },
             {
                 title: "Correo",
                 dataIndex: "mail",
                 key: "mail",
-                width: "45%",
+                width: "40%",
                 ...this.getColumnSearchProps("mail", "correo")
+            },
+            {
+                title: "Eliminar",
+                width: "10%",
+                render: () => (
+                    <span>
+                      <a href="google.com">Borrar</a>
+                    </span>
+                  ),
             }
         ]
 
-        return (
+      return (
             <Table
             dataSource={this.state.dataSource}
             columns={columns}
             bordered={true}
             expandedRowRender={record => (
-              <div>
-                <div>
-                  <b>Fecha de radicación:</b> {record.date}.
-                </div>
-                <div>
-                  <b>Respuesta de Consejo de Facultad:</b> {record.approval_status}.
-                </div>
-                <div>
-                  <b>Respuesta de Comité Asesor:</b> {record.advisor_response}.
-                </div>
-                <div>
-                  <b>Instancia que decide:</b> {record.decision_maker}.
-                </div>
-                <div>
-                  <b>ID del caso:</b> {record.id}.
-                </div>
-              </div>
+              <Form onSubmit={this.handleSubmit}>
+              <Form.Item label="Tipo de usuario">
+                <Radio.Group
+                  defaultValue="Duda"
+                  buttonStyle="solid"
+                  onChange={this.handleFormLayoutChange}
+                >
+                  <Radio.Button value="NoRol">Sin rol asignado</Radio.Button>
+                  <Radio.Button value="Admin">Administrador</Radio.Button>
+                  <Radio.Button value="Auxil">Auxiliar</Radio.Button>
+                  <Radio.Button value="Coord">Coordinador</Radio.Button>
+                  <Radio.Button value="UAPA">UAPA</Radio.Button>
+                  <Radio.Button value="Depen">Dependencia</Radio.Button>
+                </Radio.Group>
+                </Form.Item>
+                </Form>
             )}
             rowKey="id"
             pagination={{
