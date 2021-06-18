94aca60afa85c18aeaa5de94e6d8bf9c2097c140
jacobo4 <chandas.2001@gmail.com>
2021-06-17 19:41:11 -0500

    Hope deploy

--- src/js/Basics/Backend.js
+++ src/js/Basics/Backend.js
@@ -1,7 +1,13 @@
 export default class Backend {
+  // 1) es para despliegue
+  // 2) la segunda y la tercera es para trabajar local
+
+  // For deploy
   static backEndUrl = "https://www.ingenieria.bogota.unal.edu.co/uapapp_api/";
+
+  //For development
   //static backEndUrl = "http://localhost:3000/";
-  static newBackend = "http://168.176.26.91:8002/uapapp/";
+  // static newBackend = "http://168.176.26.91:8002/uapapp/";
 
   static openLink(url) {
     window.open(this.backEndUrl + url, "_blank");
@@ -29,7 +35,7 @@ export default class Backend {
   }
 
   static _request(method, path, headers, body) {
-    let newurl = this.newBackend + path;
+    let newurl = this.backEndUrl + path;
     let answer = fetch(newurl, {
       method: method,
       headers: headers,
